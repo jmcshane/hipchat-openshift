0d83f65130651908bf156cd32d94831b41a6874e
c5403 <james.p.mcshane@healthpartners.com>
2017-10-27 13:50:06 -0500

    Correct binary name

--- install_and_deploy.sh
+++ install_and_deploy.sh
@@ -2,6 +2,6 @@
 set -e
 go test ./...
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build .
-mv http-server docker/
+cp hipchat-openshift docker/http-server
 oc start-build openshift-bot --from-dir=docker/
 rm docker/http-server
