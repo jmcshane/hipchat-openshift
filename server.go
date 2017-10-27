a58c865627c9a7fc9804552f9b42f4a554f3a5de
c5403 <james.p.mcshane@healthpartners.com>
2017-10-26 21:02:51 -0500

    Support only post method

--- server.go
+++ server.go
@@ -2,24 +2,32 @@ package main
 
 import (
 	"bytes"
-    "fmt"
-    "log"
-    "net/http"
-    "os/exec"
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    cmd := exec.Command("oc", "get", "pods")
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		log.Fatal(err)
+type requestHandler struct{}
+
+func (handler requestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		cmd := exec.Command("oc", "get", "pods")
+		var out bytes.Buffer
+		cmd.Stdout = &out
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(w, "%s", out.String())
+	default:
+		http.Error(w, fmt.Sprintf("Method %s not supported", r.Method), 404)
 	}
-	fmt.Fprintf(w, "%s", out.String())
 }
 
 func main() {
-    http.HandleFunc("/foo", handler)    
-    log.Fatal(http.ListenAndServe(":8080", nil))    
+	handler := requestHandler{}
+	http.Handle("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
