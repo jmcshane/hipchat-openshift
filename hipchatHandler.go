22c164e73d202960fc2cff5ba9c47f0b64a2ee4d
c5403 <james.p.mcshane@healthpartners.com>
2017-10-27 11:49:55 -0500

    Remove sample data

--- hipchatHandler.go
+++ hipchatHandler.go
@@ -11,31 +11,6 @@ import (
 	"time"
 )
 
-var (
-	sampleData = `{
-		event: 'room_message',
-		item: {
-			message: {
-				date: '2015-01-20T22:45:06.662545+00:00',
-				from: {
-					id: 1661743,
-					mention_name: 'Blinky',
-					name: 'Blinky the Three Eyed Fish'
-				},
-				id: '00a3eb7f-fac5-496a-8d64-a9050c712ca1',
-				mentions: [],
-				message: '/oc get pods',
-				type: 'message'
-			},
-			room: {
-				id: 1147567,
-				name: 'The Weather Channel'
-			}
-		},
-		webhook_id: 578829
-	}`
-)
-
 //HipchatHandler Handle hipchat POST messages from slash command
 type HipchatHandler struct {
 	tokenService *TokenService
